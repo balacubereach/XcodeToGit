6ebc72a5c8f0ceae17f50c7fabbaf243a7488612
Cube Reach Technologies <cubereachtechnologies@cubes-mbp.home>
2016-09-01 16:24:25 +0530

    Initial Commit

---
+++ XcodeToGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcodeToGit
+//
+//  Created by Cube Reach Technologies on 01/09/16.
+//  Copyright © 2016 Cube Reach Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XcodeToGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XcodeToGit
+//
+//  Created by Cube Reach Technologies on 01/09/16.
+//  Copyright © 2016 Cube Reach Technologies. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
